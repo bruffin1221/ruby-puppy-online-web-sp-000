b6147b88231e16683312272a1b6d91a328c51acb
Bruce Ruffin <bruffin1221@gmail.com>
2019-10-24 19:22:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog
+
+    attr_accessor :name
+
+    def self.clear_all
+      @@all.clear
+    end
+
+    def self.print_all
+      self.all.each do |dogs|
+        puts "#{dogs}"
+      end
+    end
+
+    private
+
+    @@all=[]
+
+    def initialize(name)
+      @name=name
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+end
